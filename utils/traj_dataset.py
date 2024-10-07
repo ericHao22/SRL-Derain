c45de4afd7eac7f6149e005b3804111a8a8748a6
Eric Chang <ericsonc510@gmail.com>
2024-10-07 18:25:31 +0800

    chore(util): [traj_dataset] change to sample relative trajectory pair

--- utils/traj_dataset.py
+++ utils/traj_dataset.py
@@ -3,20 +3,20 @@ import numpy as np
 
 class TrajectoryDataset(object):
     """
-    Given a set of trajectory, provide a preference between every possible pair of trajectories
+    Given a set of trajectory, provide a preference between every possible relative pair of trajectories
     """
 
     def __init__(self):
-        self.trajs = []
-
-    def add_traj(self, new_traj):
-        self.trajs.append(new_traj)
+        self.traj_pairs = []
+    
+    def add_traj_pair(self, forward_traj, backward_traj):
+        self.traj_pairs.append([forward_traj, backward_traj])
 
     def len(self):
-        return len(self.trajs)
+        return len(self.traj_pairs) * 2
     
-    def reset_trajs(self):
-        self.trajs = []
+    def reset(self):
+        self.traj_pairs = []
     
     def batch_sample(self, batch_size):
         #########################
@@ -24,10 +24,11 @@ class TrajectoryDataset(object):
         # tau_is, tau_js: [B, c, h, w]
         # R1_bs, R2_bs: [B, 1]
         def sample():
-            N = len(self.trajs)
-            a, b = np.random.choice(N,2,replace=False)
-            x1, x2 = self.trajs[a].states, self.trajs[b].states
-            R1_b, R2_b = np.sum(self.trajs[a].reward_brisque), np.sum(self.trajs[b].reward_brisque)
+            N = len(self.traj_pairs)
+            index = np.random.choice(N, 1, replace=False)[0]
+            forward, backward = self.traj_pairs[index]
+            x1, x2 = forward.states, backward.states
+            R1_b, R2_b = np.sum(forward.reward_brisque), np.sum(backward.reward_brisque)
             return x1, x2, R1_b, R2_b
 
         tau_is = []; tau_js = []; R1_bs = []; R2_bs = []
